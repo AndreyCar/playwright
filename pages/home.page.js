46b6dd6dd32e22bff0498d4c2d8cd9f3ef631b77
AndreyCar <kravchenko.svg@gmail.com>
2022-07-13 19:03:01 +0300

    first push

---
+++ pages/home.page.js
@@ -0,0 +1,34 @@
+const { expect } = require('@playwright/test');
+
+exports.HomePage = class HomePage {
+	constructor(page) {
+		this.page = page;
+		this.getCookiesCloseButton = page.locator('.sc-df34c536-5.jRrWoh');
+		this.getMenuList = page.locator('.sc-7b3980dc-6.gdETaM');
+		this.getElasticSipButton = page.locator('.sc-7b3980dc-2.bjOBlW [href="/pricing/elastic-sip"]');
+	}
+
+	async goto() {
+		await this.page.goto('https://telnyx.com/');
+	}
+
+	async toBeVisibleEnabled(element) {
+		await expect(element).toBeVisible();
+		await expect(element).toBeEnabled();
+	}
+
+	async getMenuListHover() {
+		await this.getMenuList.last().hover();
+	}
+
+	async elasticSipButtonClick() {
+		await this.toBeVisibleEnabled(this.getElasticSipButton);
+		await this.getElasticSipButton.click();
+		await expect(this.page).toHaveURL('https://telnyx.com/pricing/elastic-sip');
+	}
+
+	async cookiesCloseButtonClick() {
+		await this.toBeVisibleEnabled(this.getCookiesCloseButton);
+		await this.getCookiesCloseButton.click();
+	}
+};
